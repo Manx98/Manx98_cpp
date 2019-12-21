55cdba1405e107fcc63fc826ee8d1040d7a71ef1
Manx98 <wenyiyu98@gmail.com>
2019-12-21 12:05:50 +0800

    L1-031 修改正确

--- L1-031.cpp
+++ L1-031.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <cmath>
-#include <windows.h>
 using namespace std;
 int main()
 {
     int n, w, h;
+    cin>>n;
     while (n--)
     {
         cin >> h >> w;
-        if (fabs(w/2 - (h - 100) * 0.9) <= (h - 100) * 0.9 * 0.1)
-            cout << "You are wan mei!";
-        else if (w/2  > (h - 100) * 0.9)
-            cout << "You are tai pang le!";
+        if (fabs(w - (h - 100) * 0.9*2) < (h - 100) * 0.9 * 0.1*2)
+            cout << "You are wan mei!\n";
+        else if (w  >= (h - 100) * 0.9*2)
+            cout << "You are tai pang le!\n";
         else
-            cout << "You are tai shou le!";
+            cout << "You are tai shou le!\n";
     }
 
-    system("pause");
 }
